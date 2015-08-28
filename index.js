d61104b5393af8639e5f7f954466ba18a9d86269
Danilo S. Morães <greendhulke@gmail.com>
2015-08-28 14:14:33 -0300

    Mais uma mudança para o openshift

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
 var express = require('express');
 var app = express();
 
+app.set('port', process.env.OPENSHIFT_NODEJS_PORT || process.env.PORT || 3002);
+app.set('ip', process.env.OPENSHIFT_NODEJS_IP || "127.0.0.1");
+
 app.get('/', function (req, res) {
   res.send('Hello World!');
 });
 
-var server = app.listen(8080, function () {
+var server = app.listen(app.get('port') ,app.get('ip'), function () {
   var host = server.address().address;
   var port = server.address().port;
 
