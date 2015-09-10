8644c99d5d64b9b36b4c7fc1e26c01e6aa302658
Danilo S. Morães <greendhulke@gmail.com>
2015-09-10 09:35:33 -0300

    upload funcionando

--- index.js
+++ index.js
@@ -1,12 +1,20 @@
+var BASE_PATH = "127.0.0.1";
+var BASE_PORT = 3002;
+
 var path = require('path');
 var express = require('express');
 var multer  = require('multer')
-var upload = multer({ dest: 'uploads/' })
+var upload = multer({ dest: 'imagens/' })
+var fs = require('fs');
 var app = express();
 
-app.set('port', process.env.OPENSHIFT_NODEJS_PORT || process.env.PORT || 3002);
-app.set('ip', process.env.OPENSHIFT_NODEJS_IP || "127.0.0.1");
+app.set('port', process.env.OPENSHIFT_NODEJS_PORT || process.env.PORT || BASE_PORT);
+app.set('ip', process.env.OPENSHIFT_NODEJS_IP || BASE_PATH);
 app.set('view engine', 'ejs');
+
+express.static.mime.default_type = "image/jpeg";
+app.use('/imagens', express.static(__dirname + '/imagens'));
+
 app.get('/', function (req, res) {
 	res.type('application/json');
 	res.sendFile(path.join(__dirname + '/movies.json'), {headers: {
@@ -17,7 +25,22 @@ app.get('/', function (req, res) {
 app.get('/upload', function (req, res){
     res.render('upload');
 });
+
 app.post('/upload', upload.single('file'), function (req, res, next) {
+	
+	var obj, msg;
+	fs.readFile('movies.json', 'utf8', function (err, data) {
+	  if (err) throw err;
+	  msg = req.body.msg;
+	  obj = JSON.parse(data);
+	  console.log(req.file.filename + ' - ' + msg);
+	  obj.push({title:msg, image:'http://'+BASE_PATH+':'+BASE_PORT+"/imagens/"+req.file.filename});
+		console.log(obj);
+		fs.writeFile('movies.json', JSON.stringify(obj), function (err) {
+		  if (err) throw err;
+		  console.log('It\'s saved!');
+		});
+	  });
   res.end('uploaded');
 })
 
